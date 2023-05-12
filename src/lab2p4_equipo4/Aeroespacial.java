d212126e58ec02ed07de2a1dbaeaae405135cdf1
Fredy Salvador <Fredy Salvador@DESKTOP-N10PBLA>
2023-05-12 15:41:14 -0600

    7 Avance Declaraciones

--- src/lab2p4_equipo4/Aeroespacial.java
+++ src/lab2p4_equipo4/Aeroespacial.java
@@ -13,14 +13,32 @@ import java.util.List;
 public class Aeroespacial extends Transportes{
 
 private String Tipo_comb;
-
-
-
-
-
+private boolean combatop;
 
     public Aeroespacial(){ 
     super();
+    }
+
+    public Aeroespacial(String Tipo_comb, boolean combatop, Double capcTant, Double CantMaxKmTanLle, List<String> Primates) {
+        super(capcTant, CantMaxKmTanLle, Primates);
+        this.Tipo_comb = Tipo_comb;
+        this.combatop = combatop;
+    }
+
+    public String getTipo_comb() {
+        return Tipo_comb;
+    }
+
+    public void setTipo_comb(String Tipo_comb) {
+        this.Tipo_comb = Tipo_comb;
+    }
+
+    public boolean isCombatop() {
+        return combatop;
+    }
+
+    public void setCombatop(boolean combatop) {
+        this.combatop = combatop;
     }
 
    
