b392865f7d9d901304dc8a8a4866aee27ed1111e
Fredy Salvador <Fredy Salvador@DESKTOP-N10PBLA>
2023-05-12 14:01:49 -0600

    Menu

--- src/lab2p4_equipo4/Lab2P4_Equipo4.java
+++ src/lab2p4_equipo4/Lab2P4_Equipo4.java
@@ -4,6 +4,8 @@
  */
 package lab2p4_equipo4;
 
+import javax.swing.JOptionPane;
+
 /**
  *
  * @author Fredy Salvador
@@ -15,6 +17,44 @@ public class Lab2P4_Equipo4 {
      */
     public static void main(String[] args) {
         // TODO code application logic here
+        
+        int opcion = 0;
+        
+        while (opcion != 6){
+        opcion = Integer.parseInt(JOptionPane.showInputDialog("1-Area de Seres Vivos\n2-Area de Planetas\n3-Area de TRansporte\n4-Inicio de Sesion\n5-Salir"));
+        
+        if(opcion == 1){
+        
+        
+        
+        }else if (opcion == 2){
+        
+        
+        
+        }else if (opcion == 3){
+        
+        
+        
+        }else if (opcion == 4){
+        
+        
+        
+        }else if (opcion == 5){
+        
+        
+        
+        }else if (opcion == 6){
+        
+        
+        
+        }
+        
+        
+        
+        
+        }
+        
+       
     }
     
 }
