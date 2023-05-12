06bd2126d716d65db6d3d8e01d9d81b07035acff
Fredy Salvador <Fredy Salvador@DESKTOP-N10PBLA>
2023-05-12 13:32:21 -0600

    Inicio del Proyecto

---
+++ src/lab2p4_equipo4/Lab2P4_Equipo4.java
@@ -0,0 +1,20 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Main.java to edit this template
+ */
+package lab2p4_equipo4;
+
+/**
+ *
+ * @author Fredy Salvador
+ */
+public class Lab2P4_Equipo4 {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+    }
+    
+}
